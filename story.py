a75f4638284a94ebcc3488645d8cf10d9336bc0c
Hassan Khan <khanmuhammadhassan2008@gmail.com>
2023-07-13 21:22:16 +0500

    Took Info About Player

--- story.py
+++ story.py
@@ -1,2 +1,10 @@
 def startGame():
-    input("What is your Characters name? ")
+    info = []
+    
+    heroName = input("What is your Characters name? ")
+    info.append(heroName)
+
+    enemyName = input("Who is the enemy of the game? ")
+    info.append(enemyName)
+
+    
